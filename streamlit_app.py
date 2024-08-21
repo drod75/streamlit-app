d85ee7cd0d76f11f49cef7c8606eb3af4202911a
drod75 <dr507498@gmail.com>
2024-08-21 12:10:15 -0400

    'edits'

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.title('Streamlit Portfolio')
-st.text('''This is the first few edits to the streamlit repo I am experimenting with, further pages will be 
-        made that help dive into streamlit.''')
+st.text('''This is the first few edits to the streamlit repo I am experimenting with, further pages will be made that help dive into streamlit.''')
 
 st.text('Paragraph seperation experiment.')
