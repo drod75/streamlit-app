a152098f08ff4220c02597ae9eff706ad1bf8c0e
drod75 <dr507498@gmail.com>
2024-09-25 16:40:07 -0400

    streamlit_edits

--- python_pages/main.py
+++ python_pages/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_login.authentication import *
 with open( "app/styles/fonts.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html=True)
 st.markdown('# Streamlit App')
--- python_pages/welcome.py
+++ python_pages/welcome.py
@@ -1,4 +1,6 @@
 import streamlit as st
-with open( "app/styles/fonts.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html=True)
-st.markdown('# Welcome!')
+with open("app/styles/fonts.css") as css:
+    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+with open('python_pages/writing/welcome.txt') as file:
+    for line in file:
+        st.markdown(line)
